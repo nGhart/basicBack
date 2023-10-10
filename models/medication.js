d800f33ad84bdfcac75801ced626d4060c9ee094
nGhart <107068059+nGhart@users.noreply.github.com>
2023-10-10 07:31:27 +0000

    Update medication.js

--- models/medication.js
+++ models/medication.js
@@ -5,6 +5,7 @@ const medSchema = new mongoose.Schema({
   number: { type: String, required: false },
   expiryDate: { type: String, required: true },
   supplier: { type: String, required: false },
+  status: { type: String, required: false, default: 'Available' },
 });
 
 const Med = mongoose.model('Med', medSchema);
